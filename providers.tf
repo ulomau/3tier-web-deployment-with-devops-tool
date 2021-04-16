ff3abd75d08f994b2e3d92380b631a6eb6bb1b51
Uloma <66249264+ulomau@users.noreply.github.com>
2021-04-16 07:00:32 -0700

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,11 +1,9 @@
 provider "google" {
-  credentials = "GOOGLE_CREDENTIALS"
   project     = "terraform-310215"
   region      = "us-central1"
   zone        = "us-central1-a"
 }
 provider "google-beta" {
-  credentials = "GOOGLE_CREDENTIALS"
   project     = "terraform-310215"
   region      = "us-central1"
   zone        = "us-central1-a"
